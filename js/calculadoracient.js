34ba57d532b660521afdb58082382c145d5a668b
lara2006silveira <bresolinfernanda3@gmail.com>
2023-03-23 15:12:16 -0300

    Meu projeto

---
+++ js/calculadoracient.js
@@ -0,0 +1,30 @@
+function cos(){
+    a = parseInt(documentf1.val1.value) * (MathPI/180);
+    document.f1.val2.value=Math.cos(a);
+}
+function sin(){
+    a = parseInt(document.f1.val1.value) * (MathPI/180);
+    document.f1.val2.value=Math.sin(a);
+}
+function tan(){
+    a = parseInt(document.f1.val1.value) * (MathPI/180);
+    document.f1.val2.value=Math.tan(a);
+}
+function sqrt(){
+    a = document.f1.val1.value; 
+    document.f1.val2.value=Math.sqrt(parseInt(a));
+}
+function log(){
+    a = document.f1.val1.value;
+    document.f1.val2.value=Math.log(parseInt(a));
+}
+function exp(){
+    a = document.f1.val1.value;
+    document.f1.val2.value=Math.exp(parseInt(a));
+}
+
+
+
+
+
+
